51d7efa6b71d66ed1a021f77797b73361e3e8818
andresdiazv <adiaz616@fiu.edu>
2023-07-02 21:48:34 -0400

    changes to client and server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ users: ["user1", "user2", "user3"] });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // server run on port 5000, client run on port 3000
