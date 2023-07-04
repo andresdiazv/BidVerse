5e5db0686ad1223fa15f3c398bec92d71802d14d
andresdiazv <adiaz616@fiu.edu>
2023-07-03 22:00:58 -0400

    changes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import Home from './pages/Home';
-import Auctions from './pages/Auctions';
-import AuctionDetail from './pages/AuctionDetails';
-import NewAuction from './pages/NewAuction';
-import Auth from './pages/Auth';
+import React from "react";
+import Home from "./pages/Home";
+import Auctions from "./pages/Auctions";
+import AuctionDetail from "./pages/AuctionDetails";
+import NewAuction from "./pages/NewAuction";
+import Auth from "./pages/Auth";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Home />
-      <Auctions />
-      <AuctionDetail />
-      <NewAuction />
-      <Auth />
+      <Router>
+        <Route>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auctions" element={<Auctions />} />
+            <Route path="/auctions/:id" element={<AuctionDetail />} />
+            <Route path="/new-auction" element={<NewAuction />} />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
+        </Route>
+      </Router>
     </div>
   );
 }
